48b4dfe5d728aa437abcdae8b4a0567d93200396
molzania <molzania_1507@yahoo.co.id>
2021-08-03 11:02:22 +0700

    commit on 'main'

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let API1 = "https://api.kawalcorona.com/indonesia";
-let API2 = "https://api.kawalcorona.com/indonesia/provinsi";
-let API3 = "https://api.kawalcorona.com/";
+let API1 = "https://covid19.mathdro.id/api/countries/indonesia";
+let API2 = "https://covid19.mathdro.id/api/countries";
+let API3 = "https://covid19.mathdro.id/api/daily";
 
-fetch(API1, {method: "GET"})
+fetch(API1, {method: "POST"})
 .then(result1 => result1.json())
 .then(result1 => {
     console.log(result1)
@@ -11,7 +11,7 @@ fetch(API1, {method: "GET"})
     console.log(error1);
 })
 
-fetch(API2, {method: "GET"})
+fetch(API2, {method: "POST"})
 .then(result2 => result2.json())
 .then(result2 => {
     console.log(result2)
@@ -20,7 +20,7 @@ fetch(API2, {method: "GET"})
     console.log(error2);
 })
 
-fetch(API3, {method: "GET"})
+fetch(API3, {method: "POST"})
 .then(result3 => result3.json())
 .then(result3 => {
     console.log(result3)
