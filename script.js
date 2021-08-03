7aa8e8088a38b7b8944600468fd29ffe20bf92d5
molzania <molzania_1507@yahoo.co.id>
2021-08-03 11:52:30 +0700

    commit on 'main'

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let API1 = "https://covid19.mathdro.id/api/countries/indonesia";
 let API2 = " https://covid19.mathdro.id/api/";
 let API3 = "https://covid19.mathdro.id/api/confirmed";
 
+let indonesia = document.getElementById("data-covid-ina");
+let global = document.getElementById("data-covid-dunia");
+
 fetch(API1, {method: "POST"})
 .then(result1 => result1.json())
 .then(result1 => {
@@ -15,6 +18,11 @@ fetch(API2, {method: "POST"})
 .then(result2 => result2.json())
 .then(result2 => {
     console.log(result2)
+        global.innerHTML = `
+        <div id="konfirmasi">
+        <h3>Konfirmasi</h3> 
+        <p src='${result2[0].value}'></p>
+         </div>` 
 })
 .catch(error2 => {
     console.log(error2);
@@ -24,6 +32,12 @@ fetch(API3, {method: "POST"})
 .then(result3 => result3.json())
 .then(result3 => {
     console.log(result3)
+    for (let b=0;b<=23;b++){
+        countriesContainer.innerHTML += `
+        <div id="flag"> 
+        <img src='${result3[b].flag}'/>
+        <span>${result3[b].name}</span>
+         </div>` }
 })
 .catch(error3 => {
     console.log(error3);
