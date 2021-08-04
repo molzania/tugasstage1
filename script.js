172004805860f913bb77c2b60eca7adfac068039
molzania <molzania_1507@yahoo.co.id>
2021-08-04 15:55:28 +0700

    commit on 'main'

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let API1 = "https://covid19.mathdro.id/api/countries/indonesia";
 let API2 = "https://covid19.mathdro.id/api/";
 
-let indonesia = document.getElementById("data-covid-ina");
-let global = document.getElementById("data-covid-dunia");
+let indonesiaConfirmed = document.getElementById("value-konfirmasi-indo");
+let indonesiaRecovered = document.getElementById("value-sembuh-indo");
+let indonesiaDeaths = document.getElementById("value-meninggal-indo");
+let globalConfirmed = document.getElementById("value-konfirmasi-dunia");
+let globalRecovered = document.getElementById("value-sembuh-dunia");
+let globalDeaths = document.getElementById("value-meninggal-dunia");
 
 fetch(API1, {method: "GET"})
 .then(result1 => result1.json())
 .then(result1 => {
     console.log(result1)
+indonesiaConfirmed.innerHTML = `${result1.confirmed.value}`
 })
 .catch(error1 => {
     console.log(error1);
