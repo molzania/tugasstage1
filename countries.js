0196c16a7ac215cc7e79f258d09891e46b4ae3ec
molzania <molzania_1507@yahoo.co.id>
2021-08-04 19:56:00 +0700

    commit on 'main'

--- countries.js
+++ countries.js
@@ -31,20 +31,3 @@ fetch(API1, {method: "GET"})
     console.log(error);
 })
 
-async function getCountryName(name) {
-    const url = API1 + name;
-    const response = await fetch(url);
-    const responseData = await response.json();
-  
-    console.log(responseData);
-  }
-  
-  function watchForm() {
-    $('form').submit(event => {
-      event.preventDefault();
-      const searchTerm = $('#data-container').val();
-      getCountryName(searchTerm);
-    });
-  }
-  
-  $(watchForm);
