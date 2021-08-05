f522d7266100f17b786b05afe1d7dfb9cf4c9534
molzania <molzania_1507@yahoo.co.id>
2021-08-05 12:09:59 +0700

    commit on 'main'

--- countries.js
+++ countries.js
@@ -42,7 +42,7 @@ function searchByName() {
     display.innerHTML = "";
 
     for(let a=0;a<=180;a++){
-      if(regex.test(result[a]){
+      if(regex.test(result[a])){
         display.innerHTML += `
         <h3>${result[a].countryRegion}</h3>
         <div id="data-negara">
@@ -61,4 +61,4 @@ function searchByName() {
         </div>
         `}
 return false;
-      }}
+    }}
