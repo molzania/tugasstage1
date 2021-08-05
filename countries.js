1619d5299b9310bed58b417553d3b48f829a702a
molzania <molzania_1507@yahoo.co.id>
2021-08-05 10:41:01 +0700

    commit on 'main'

--- countries.js
+++ countries.js
@@ -1,12 +1,14 @@
 let API1 = "https://covid19.mathdro.id/api/confirmed";
 
 let countriesContainer = document.getElementById("data-container");
+let country =[];
 
 fetch(API1, {method: "GET"})
 .then(result => result.json())
 .then(result => {
     console.log(result)
     for (let i=0;i<=180;i++){
+        country.push(result[i])
         countriesContainer.innerHTML += `
         <h3>${result[i].countryRegion}</h3>
         <div id="data-negara">
@@ -30,16 +32,40 @@ fetch(API1, {method: "GET"})
     console.log(error);
 })
 
+let Cari = document.getElementById("cari");
+Cari.onclick = searchByName;
+
 function searchByName() {
+
     let regex2=/^[A-Za-z0-9_.]+$/
     textInput = document.getElementById("search").value;
     display = document.getElementById("data-container");
+    display.innerHTML = "";
 
-    for(let i=0;i<=180;i++){
-        result[i].countryRegion;
-        regex2.test(textInput);
-        if(true){
-            display.innerHTML = regex.test(textInput)
-        }
+    for(let a=0;a<=180;a++){
+    
+      regex2.test(textInput);
+      
+      if(true){
+        display.innerHTML += `
+        <h3>${result[a].countryRegion}</h3>
+        <div id="data-negara">
+        <div id="konfirmasi">
+        <h4>Konfirmasi</h4>
+        <p>${result[a].confirmed}</p>
+        </div>
+        <div id="sembuh">
+        <h4>Sembuh</h4>
+        <p>${result[a].recovered}</p>
+        </div>
+        <div id="meninggal">
+        <h4>Meninggal</h4>
+        <p>${result[a].deaths}</p>
+        </div>
+        </div>
+        `
     }
+   }
+return false;
+
 }
