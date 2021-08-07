b8b24b9ebc39ae8712ebb7d8f475ec4a8d8d8ee9
molzania <molzania_1507@yahoo.co.id>
2021-08-07 14:40:54 +0700

    commit on 'main'

--- countries.js
+++ countries.js
@@ -35,31 +35,66 @@ fetch(API1, {method: "GET"})
 let Cari = document.getElementById("cari");
 Cari.onclick = searchByName;
 
-function searchByName(){
+// function searchByName(){
     
+//     let display = document.getElementById("data-container");
+//     let textInput = document.getElementById("search").value;
+//     display.innerHTML = "";
+//     let regex = new RegExp(textInput, 'igm');
+//     for(let i=0;i<country.length;i++){
+//       if(regex.test(`${country[i].countryRegion}`)){
+//         display.innerHTML += `
+//         <h3>${country[i].countryRegion}</h3>
+//         <div id="data-negara">
+//         <div id="konfirmasi">
+//         <h4>Konfirmasi</h4>
+//         <p>${country[i].confirmed}</p>
+//         </div>
+//         <div id="sembuh">
+//         <h4>Sembuh</h4>
+//         <p>${country[i].recovered}</p>
+//         </div>
+//         <div id="meninggal">
+//         <h4>Meninggal</h4>
+//         <p>${country[i].deaths}</p>
+//         </div>
+//         </div>
+//         `}
+//     }
+//     return false;
+// }
+
+function searchByName(event) {
+    event.preventDefault();
     let display = document.getElementById("data-container");
     let textInput = document.getElementById("search").value;
     display.innerHTML = "";
-    let regex = new RegExp(textInput, 'igm');
-    for(let i=0;i<country.length;i++){
-      if(regex.test(`${country[i].countryRegion}`)){
+    
+    for(let i=0; i < country.length; i++){
+      if (country[i].countryRegion.toLowerCase().includes(textInput.toLowerCase())) {
+        console.log('test');
         display.innerHTML += `
-        <h3>${country[i].countryRegion}</h3>
-        <div id="data-negara">
-        <div id="konfirmasi">
-        <h4>Konfirmasi</h4>
-        <p>${country[i].confirmed}</p>
-        </div>
-        <div id="sembuh">
-        <h4>Sembuh</h4>
-        <p>${country[i].recovered}</p>
-        </div>
-        <div id="meninggal">
-        <h4>Meninggal</h4>
-        <p>${country[i].deaths}</p>
-        </div>
-        </div>
-        `}
+          <h3>${country[i].countryRegion}</h3>
+          <div id="data-negara">
+          <div id="konfirmasi">
+          <h4>Konfirmasi</h4>
+          <p>${country[i].confirmed}</p>
+          </div>
+          <div id="sembuh">
+          <h4>Sembuh</h4>
+          <p>${country[i].recovered}</p>
+          </div>
+          <div id="meninggal">
+          <h4>Meninggal</h4>
+          <p>${country[i].deaths}</p>
+          </div>
+          </div>
+        `;
+      }
     }
-    return false;
-}
+    
+    if (display.innerHTML.length === 0) {
+      // Error nya disini
+      alert('Data tidak ditemukan. Coba lagi.');
+    }
+  }
